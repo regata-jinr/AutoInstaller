79b7df6027647a153d54bf730b4962a1da02dd6d
Boris Rumyantsev <bdrum@jinr.ru>
2020-03-17 15:29:26 +0300

    Add output and fixes

--- src/Form1.Designer.cs
+++ src/Form1.Designer.cs
@@ -32,6 +32,7 @@
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.radioButtonEng = new System.Windows.Forms.RadioButton();
             this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -71,23 +72,35 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(135, 86);
+            this.button1.Location = new System.Drawing.Point(12, 157);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(88, 23);
+            this.button1.Size = new System.Drawing.Size(211, 23);
             this.button1.TabIndex = 2;
             this.button1.Text = "Install";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(12, 83);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(211, 61);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
             // Form1
             // 
+            this.AcceptButton = this.button1;
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(227, 277);
+            this.ClientSize = new System.Drawing.Size(230, 181);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.groupBox1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Regata Auto Installer";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
@@ -100,6 +113,7 @@
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.RadioButton radioButtonEng;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- src/Form1.cs
+++ src/Form1.cs
@@ -15,12 +15,12 @@ namespace AutoInstaller
     {
         private readonly GitHubClient _client;
         private readonly Dictionary<string, string> _reposList;
-        public Form1(string token)
+        public Form1()
         {
             InitializeComponent();
 
             _client = new GitHubClient(new ProductHeaderValue("regata-jinr"));
-            var tokenAuth = new Credentials(token);
+            var tokenAuth = new Credentials("");
             _client.Credentials = tokenAuth;
 
             _reposList = new Dictionary<string, string>
@@ -33,17 +33,19 @@ namespace AutoInstaller
             Directory.CreateDirectory($"{AppContext.BaseDirectory}\\.temp");
         }
 
-
         private async void DownloadLatestSetupExe(string repo)
         {
+            label1.Text = "Start downloading of files:";
             var release = await _client.Repository.Release.GetLatest("regata-jinr", repo);
             var asset = release.Assets.Where(a => a.Name == "Setup.exe").Last();
 
             using (var client = new WebClient())
             {
                 client.DownloadFile(asset.Url, $".temp\\{_reposList[repo]}Setup.exe");
+                label1.Text += $"{Environment.NewLine}{_reposList[repo]}Setup.exe downloaded successfuly";
             }
 
+            label1.Text = $"Start installation process:{Environment.NewLine}";
             RunCommand($"{_reposList[repo]}Setup.exe");
 
         }
@@ -60,34 +62,59 @@ namespace AutoInstaller
                 process.StartInfo.RedirectStandardError = true;
                 process.StartInfo.CreateNoWindow = true;
                 process.StartInfo.WorkingDirectory = $"{AppContext.BaseDirectory}\\.temp";
+                process.OutputDataReceived += Process_OutputDataReceived;
                 process.Start();
             }
         }
 
-
+        private void Process_OutputDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            label1.Text += $"{Environment.NewLine}{e.Data}";
+        }
 
         private async void GetFilesFromArchive()
         {
+
+            label1.Text = $"Download genie2k files:{Environment.NewLine}";
             var release = await _client.Repository.Release.GetLatest("regata-jinr", "");
             var asset = release.Assets.Where(a => a.Name == "sources.zip").Last();
 
             using (var client = new WebClient())
                 client.DownloadFile(asset.Url, $".temp\\sources.zip");
 
+            if (File.Exists(@".temp\sources.zip"))
+                label1.Text += $"Downloading complete";
+
+
+            label1.Text = $"Expanding archive:{Environment.NewLine}";
+
             RunCommand("Expand-Archive", @"-Path .\sources.zip .");
 
             if (!Directory.Exists(@".temp\sources"))
+            {
+                label1.Text += $"Archive file not found";
                 throw new DirectoryNotFoundException("Directory with sources files not found");
+            }
             else
                 File.Delete(@".temp\sources.zip");
 
 
+            label1.Text = $"Copying of WORKPROG folder to C";
             Directory.Move(@".temp\WORKPROG", @"C:\");
+
+            if (!Directory.Exists(@"C:\WORKPROG"))
+            {
+                label1.Text += $"WORKPROG dir not found in disk C after copying";
+                throw new DirectoryNotFoundException("Directory WORKPROG not found");
+            }
+
 
             if (radioButtonRus.Checked)
                 MoveAndRenameIssFiles("Rus");
             else
                 MoveAndRenameIssFiles("Eng");
+
+            label1.Text = $"Starting of installation Genie2K:{Environment.NewLine}";
 
             RunCommand(@"sources\S504V3.2.1\\setup.exe", "/s");
             RunCommand(@"sources\S501V3.2.1\\setup.exe", "/s");
