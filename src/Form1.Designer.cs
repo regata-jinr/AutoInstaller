4a9582894442bb1d86b4b2f80cbc33e412b9fc90
Boris Rumyantsev <bdrum@jinr.ru>
2020-03-17 14:39:53 +0300

    Regata auto installer for genie2k

---
+++ src/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace AutoInstaller
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.radioButtonRus = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButtonEng = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // radioButtonRus
+            // 
+            this.radioButtonRus.AutoSize = true;
+            this.radioButtonRus.Checked = true;
+            this.radioButtonRus.Location = new System.Drawing.Point(6, 31);
+            this.radioButtonRus.Name = "radioButtonRus";
+            this.radioButtonRus.Size = new System.Drawing.Size(65, 19);
+            this.radioButtonRus.TabIndex = 0;
+            this.radioButtonRus.TabStop = true;
+            this.radioButtonRus.Text = "Russian";
+            this.radioButtonRus.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButtonEng);
+            this.groupBox1.Controls.Add(this.radioButtonRus);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(211, 68);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Choose the language of Genie2K";
+            // 
+            // radioButtonEng
+            // 
+            this.radioButtonEng.AutoSize = true;
+            this.radioButtonEng.Location = new System.Drawing.Point(123, 31);
+            this.radioButtonEng.Name = "radioButtonEng";
+            this.radioButtonEng.Size = new System.Drawing.Size(63, 19);
+            this.radioButtonEng.TabIndex = 0;
+            this.radioButtonEng.TabStop = true;
+            this.radioButtonEng.Text = "English";
+            this.radioButtonEng.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(135, 86);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(88, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Install";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(227, 277);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton radioButtonRus;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButtonEng;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ src/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System;
+using System.Linq;
+using System.IO;
+using System.Windows.Forms;
+using Octokit;
+using System.Net;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+namespace AutoInstaller
+{
+    public partial class Form1 : Form
+    {
+        private readonly GitHubClient _client;
+        private readonly Dictionary<string, string> _reposList;
+        public Form1(string token)
+        {
+            InitializeComponent();
+
+            _client = new GitHubClient(new ProductHeaderValue("regata-jinr"));
+            var tokenAuth = new Credentials(token);
+            _client.Credentials = tokenAuth;
+
+            _reposList = new Dictionary<string, string>
+            {
+                { "RegataDBC", @"https://github.com/regata-jinr/RegataDBC/releases/latest" },
+                { "CalcConc", @"https://github.com/regata-jinr/CalcConc/releases/latest"},
+                { "SearchOfStandards", @"https://github.com/regata-jinr/SearchOfStandards" }
+            };
+
+            Directory.CreateDirectory($"{AppContext.BaseDirectory}\\.temp");
+        }
+
+
+        private async void DownloadLatestSetupExe(string repo)
+        {
+            var release = await _client.Repository.Release.GetLatest("regata-jinr", repo);
+            var asset = release.Assets.Where(a => a.Name == "Setup.exe").Last();
+
+            using (var client = new WebClient())
+            {
+                client.DownloadFile(asset.Url, $".temp\\{_reposList[repo]}Setup.exe");
+            }
+
+            RunCommand($"{_reposList[repo]}Setup.exe");
+
+        }
+
+        private void RunCommand(string command, string args = "")
+        {
+            using (var process = new Process())
+            {
+                process.StartInfo.FileName = command;
+                process.StartInfo.Arguments = args;
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.RedirectStandardInput = true;
+                process.StartInfo.RedirectStandardOutput = true;
+                process.StartInfo.RedirectStandardError = true;
+                process.StartInfo.CreateNoWindow = true;
+                process.StartInfo.WorkingDirectory = $"{AppContext.BaseDirectory}\\.temp";
+                process.Start();
+            }
+        }
+
+
+
+        private async void GetFilesFromArchive()
+        {
+            var release = await _client.Repository.Release.GetLatest("regata-jinr", "");
+            var asset = release.Assets.Where(a => a.Name == "sources.zip").Last();
+
+            using (var client = new WebClient())
+                client.DownloadFile(asset.Url, $".temp\\sources.zip");
+
+            RunCommand("Expand-Archive", @"-Path .\sources.zip .");
+
+            if (!Directory.Exists(@".temp\sources"))
+                throw new DirectoryNotFoundException("Directory with sources files not found");
+            else
+                File.Delete(@".temp\sources.zip");
+
+
+            Directory.Move(@".temp\WORKPROG", @"C:\");
+
+            if (radioButtonRus.Checked)
+                MoveAndRenameIssFiles("Rus");
+            else
+                MoveAndRenameIssFiles("Eng");
+
+            RunCommand(@"sources\S504V3.2.1\\setup.exe", "/s");
+            RunCommand(@"sources\S501V3.2.1\\setup.exe", "/s");
+            RunCommand(@"sources\S506V3.2.1\\setup.exe", "/s");
+
+            Directory.Delete(@".temp");
+
+        }
+
+        private void MoveAndRenameIssFiles(string pattern)
+        {
+            var dir = new DirectoryInfo(@".temp\sources\Genie2K_v3.2.1");
+            var files = dir.GetFiles($"*{pattern}.iss");
+            var reg = new Regex(@"\d{3}");
+
+            foreach (var file in files)
+            {
+                var pack = reg.Match(file.Name).Value;
+                file.MoveTo($"S{pack}V3.2.1\\setup.iss");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            foreach (var repo in _reposList.Keys)
+                DownloadLatestSetupExe(_reposList[repo]);
+
+            GetFilesFromArchive();
+        }
+    }
+}
